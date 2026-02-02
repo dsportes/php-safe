77b4b9ce7c8495285265a2b3a935fb4ad15bbc1f
Daniel Sportes <daniel@sportes.fr>
2026-02-02 13:03:17 +0100

    db.php écrit

--- db.php
+++ db.php
@@ -51,21 +51,43 @@ function test3 ($mysqli) {
 /* Retourne l'objet safe depuis soit son id, soit son p0, soit son r0
 null si non trouvé
 */
-function getSafe ($id, $idp0r0) {
-  $idx = !isset($idp0r0) || ($idp0r0 === 0) ? 'id' : ($idp0r0 === 1 ? 'hp0' : 'hr0');
-  $stmt = $mysqli->prepare('SELECT id, lam, data FROM SAFE WHERE ' . $idx . ' = ?');
+function getBinSafe ($id) {
+  $m = 0;
+  $stmt = $mysqli->prepare('SELECT id, lam, data FROM SAFE WHERE id = ?');
   $stmt->bind_param('s', $id); 
   $stmt->execute();
   $result = $stmt->get_result();
   $row = $result->fetch_assoc();
-  if (!isset($row)) return null;
+  if (!isset($row)) {
+    $m = 1;
+    $stmt = $mysqli->prepare('SELECT id, lam, data FROM SAFE WHERE hp0 = ?');
+    $stmt->bind_param('s', $id); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    if (!isset($row)) {
+      $stmt = $mysqli->prepare('SELECT id, lam, data FROM SAFE WHERE hr0 = ?');
+      $stmt->bind_param('s', $id); 
+      $stmt->execute();
+      $result = $stmt->get_result();
+      $row = $result->fetch_assoc();
+      if (!isset($row)) {
+        return [m, null];
+      }
+    }
+  }
   $cm = currentMonth();
   if ($row['lam'] !== $cm) {
     $stmt = $mysqli->prepare('UPDATE SAFE SET lam = ? WHERE id = ?');
     $stmt->bind_param('is', $cm, $id); 
     $stmt->execute();
   }
-  return msgpack_unpack($row->data);
+  return [m, $row->data];
+}
+
+function getSafe ($id) {
+  $ret = getBinSafe($id);
+  return [$ret['m'], $ret['bin'] ? msgpack_unpack($ret['bin']) : null];
 }
 
 /* Status de création d'un safe - Permet de savoir dans quelles conditions le safe pourrait être "recréé".
@@ -84,7 +106,7 @@ function statusSafe ($id, $hp0, $hr0) {
   $result = $stmt->get_result();
   if (isset($result)) {
     $row = $result->fetch_assoc();
-    $data = $row['data'];
+    $data = msgpack_unpack($row['data']);
     $r['lm'] = isset($data['lm']) ? $data['lm'] : 0;
     if ($data['hp0'] === $hp0) $r['xp'] = true;
     else {
@@ -115,7 +137,135 @@ function statusSafe ($id, $hp0, $hr0) {
     $result3 = $stmt3->get_result();
     $r['xr'] = !isset($result3);
   }
-  return r;
+  return $r;
 }
 
+function newSafe ($safe) {
+  $id = $safe['id'];
+  $hp0 = $safe['hp0'];
+  $hr0 = $safe('hr0');
+  $safe['lm'] = time();
+  $lam = currentMonth();
+  $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE id = ?');
+  $stmt->bind_param('s', $id); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row0 = isset($result) ? $result->fetch_assoc() : null;
+  if ((isset($row0) && $row0['hp0'] !== $hp0) || ! isset($row0)) {
+    $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE hp0 = ?');
+    $stmt->bind_param('s', $hp0); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row1 = isset($result) ? $result->fetch_assoc() : null;
+    if (isset($row1)) return 1;
+  }
+  if ((isset($row0) && $row0['hr0'] !== $hr0) || ! isset($row0)) {
+    $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE hr0 = ?');
+    $stmt->bind_param('s', $hr0); 
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row2 = isset($result) ? $result->fetch_assoc() : null;
+    if (isset($row2)) return 2;
+  }
+  $data = msgpack_pack($safe);
+  if (isset($row0)) {
+    $stmt = $mysqli->prepare('INSERT INTO SAFE (id, hp0, hr0, lam, data) VALUES (?, ?, ?, ?, ?)');
+    $stmt->bind_param('sssib', $id, $hp0, $hr0, $lam, $null);
+    chunks($stmt, $data, 4);
+    $stmt->execute();
+  } else {
+    $stmt = $mysqli->prepare('UPDATE SAFE SET hp0 = @hp0, hr0 = @hr0, lam = @lam, data = @data WHERE id = @id');
+    $stmt->bind_param('ssibs', $hp0, $hr0, $lam, $null, $id);
+    chunks($stmt, $data, 3);
+    $stmt->execute();
+  }
+  return 0;
+}
+
+function restoreSafe ($safe) {
+  $id = $safe['id'];
+  $hp0 = $safe['hp0'];
+  $hr0 = $safe('hr0');
+  $safe['lm'] = time();
+  $lam = currentMonth();
+  $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE id = ?');
+  $stmt->bind_param('s', $id); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row0 = isset($result) ? $result->fetch_assoc() : null;
+  $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE hp0 = ?');
+  $stmt->bind_param('s', $hp0); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row1 = isset($result) ? $result->fetch_assoc() : null;
+  if (isset($row1) && $row1['id'] !== $id) return 1;
+  $stmt = $mysqli->prepare('SELECT id, hp0, hr0 FROM SAFE WHERE hr0 = ?');
+  $stmt->bind_param('s', $hr0); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row2 = isset($result) ? $result->fetch_assoc() : null;
+  if (isset($row2) && $row2['id'] !== $id) return 2;
+  $data = msgpack_pack($safe);
+  if (isset($row0)) {
+    $stmt = $mysqli->prepare('INSERT INTO SAFE (id, hp0, hr0, lam, data) VALUES (?, ?, ?, ?, ?)');
+    $stmt->bind_param('sssib', $id, $hp0, $hr0, $lam, $null);
+    chunks($stmt, $data, 4);
+    $stmt->execute();
+  } else {
+    $stmt = $mysqli->prepare('UPDATE SAFE SET hp0 = ?, hr0 = ?, lam = ?, data = ? WHERE id = ?');
+    $stmt->bind_param('ssibs', $hp0, $hr0, $lam, $null, $id);
+    chunks($stmt, $data, 3);
+    $stmt->execute();
+  }
+  return 0;
+}
+
+function updPRSafe ($safe) {
+  $id = $safe['id'];
+  $hp0 = $safe['hp0'];
+  $hr0 = $safe('hr0');
+  $safe['lm'] = time();
+  $lam = currentMonth();
+  $stmt = $mysqli->prepare('SELECT id FROM SAFE WHERE hp0 = ?');
+  $stmt->bind_param('s', $hp0); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row = isset($result) ? $result->fetch_assoc() : null;
+  if (isset($row) && $row['id'] !== $id) return 2;
+  $stmt = $mysqli->prepare('SELECT id FROM SAFE WHERE hr0 = ?');
+  $stmt->bind_param('s', $hr0); 
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $row = isset($result) ? $result->fetch_assoc() : null;
+  if (isset($row) && $row['id'] !== $id) return 3;
+  $data = msgpack_pack($safe);
+  $stmt = $mysqli->prepare('UPDATE SAFE SET hp0 = ?, hr0 = ?, lam = ?, data = ? WHERE id = ?');
+  $stmt->bind_param('ssibs', $hp0, $hr0, $lam, $null, $id);
+  chunks($stmt, $data, 3);
+  $stmt->execute();
+  return 0;
+}
+
+function updSafe ($safe) {
+  $id = $safe['id'];
+  $safe['lm'] = time();
+  $lam = currentMonth();
+  $data = msgpack_pack($safe);
+  $stmt = $mysqli->prepare('UPDATE SAFE SET hp0 = ?, hr0 = ?, lam = ?, data = ? WHERE id = ?');
+  $stmt->bind_param('ssibs', $hp0, $hr0, $lam, $null, $id);
+  chunks($stmt, $data, 3);
+  $stmt->execute();
+}
+
+function delSafe ($id) {
+  $stmt = $mysqli->prepare('DELETE FROM SAFE WHERE id = ?');
+  $stmt->bind_param('s', $id);
+  $stmt->execute();
+}
+
+function purgeSafes ($lam) {
+  $stmt = $mysqli->prepare('DELETE FROM SAFE WHERE lam < ?');
+  $stmt->bind_param('s', $lam);
+  $stmt->execute();
+}
 ?>
